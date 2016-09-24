e9bb5fd2adbfc8abf12a4f6c2f55f41d5e926db2
yuichi <yuichi.nakayasu@gmail.com>
2016-09-24 22:54:59 +0900

    インポートの修正

--- src/views/user/index.php
+++ src/views/user/index.php
@@ -4,11 +4,13 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
 /* @var $this yii\web\View */
-/* @var $searchModel app\models\UserSearch */
+/* @var $searchModel app\models\searches\UserSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = Yii::t('app', 'Users');
 $this->params['breadcrumbs'][] = $this->title;
+
+
 ?>
 <div class="user-index">
 
