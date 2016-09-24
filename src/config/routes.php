3d1a55d426ee95ea947a6fa7dff05dc365331669
yuichi <yuichi.nakayasu@gmail.com>
2016-09-25 00:14:03 +0900

    CRUDルーティング設定

--- src/config/routes.php
+++ src/config/routes.php
@@ -3,4 +3,11 @@ return [
     // DefaultController
     ''         => 'default/index',
     'sign-out' => 'sign-in/sign-out',
+
+    // User
+    '<_c>/view'            => '<_c>/not-found',
+    '<_c>/update'          => '<_c>/not-found',
+    '<_c>/<id:\d+>'        => '<_c>/view',
+    '<_c>/<id:\d+>/edit'   => '<_c>/update',
+    '<_c>/<id:\d+>/delete' => '<_c>/delete',
 ];
